4c02148037bca84b6f869fb55abffaea645445bc
Henrique <henrique.s.andrade1@gmail.com>
2017-05-29 13:45:10 -0300

    - inclusão do Diretor

---
+++ filme-spark-web/src/main/java/br/ufrj/mba/eng30/filme/spark/web/model/Diretor.java
@@ -0,0 +1,36 @@
+package br.ufrj.mba.eng30.filme.spark.web.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Diretor {
+	@SerializedName("director_name")
+	private String nome;
+	@SerializedName("gross")
+	private long arrecadacaoLocal;
+	@SerializedName("qtd_movies")
+	private int qtdFilmes;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getArrecadacaoLocal() {
+		return arrecadacaoLocal;
+	}
+
+	public void setArrecadacaoLocal(long arrecadacaoLocal) {
+		this.arrecadacaoLocal = arrecadacaoLocal;
+	}
+
+	public int getQtdFilmes() {
+		return qtdFilmes;
+	}
+
+	public void setQtdFilmes(int qtdFilmes) {
+		this.qtdFilmes = qtdFilmes;
+	}
+}
